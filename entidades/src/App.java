c2f0d9060c7bf0816735bd51a7402850f8517d19
DutraGames <gabrieldutraamarante@gmail.com>
2023-09-29 17:30:03 -0300

    refactore: Add Class Celula

--- entidades/src/App.java
+++ entidades/src/App.java
@@ -6,104 +6,141 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
-        
+
         // Professor
-/* 
-        Professor professor = new Professor();
-
-        System.out.print("Nome: ");
-        professor.nome = input.next();
-        System.out.print("Altura: ");
-        professor.altura = input.nextFloat();
-        System.out.print("Formacao: ");
-        professor.formacao = input.next();
-
-        System.out.println("O professor " + professor.nome + " ira ensinar!");
-        professor.ensinar();
-        System.out.println("O professor " + professor.nome + " ira avaliar!");
-        int nota1 = rand.nextInt(10);
-        int nota2 = rand.nextInt(10);
-        int nota3 = rand.nextInt(10);
-        System.out.println("Sua media foi de: " + professor.avaliar(nota1, nota2, nota3));
- */
+        /*
+         * Professor professor = new Professor();
+         * 
+         * System.out.print("Nome: ");
+         * professor.nome = input.next();
+         * System.out.print("Altura: ");
+         * professor.altura = input.nextFloat();
+         * System.out.print("Formacao: ");
+         * professor.formacao = input.next();
+         * 
+         * System.out.println("O professor " + professor.nome + " ira ensinar!");
+         * professor.ensinar();
+         * System.out.println("O professor " + professor.nome + " ira avaliar!");
+         * int nota1 = rand.nextInt(10);
+         * int nota2 = rand.nextInt(10);
+         * int nota3 = rand.nextInt(10);
+         * System.out.println("Sua media foi de: " + professor.avaliar(nota1, nota2,
+         * nota3));
+         */
 
         // Aplicativo
-/* 
-        Aplicativo app = new Aplicativo();
-
-        System.out.print("Nome: ");
-        app.nome = input.next();
-        System.out.print("Interfaces: ");
-        app.interfaces = input.next();
-        System.out.print("Linguagem: ");
-        app.linguagem = input.next();
-
-        app.abrir();
-        System.out.println("O aplicativo " + app.nome + " foi aberto!");
-
-        app.armazenarDados();
- */
+        /*
+         * Aplicativo app = new Aplicativo();
+         * 
+         * System.out.print("Nome: ");
+         * app.nome = input.next();
+         * System.out.print("Interfaces: ");
+         * app.interfaces = input.next();
+         * System.out.print("Linguagem: ");
+         * app.linguagem = input.next();
+         * 
+         * app.abrir();
+         * System.out.println("O aplicativo " + app.nome + " foi aberto!");
+         * 
+         * app.armazenarDados();
+         */
 
         // Thor
-        /* Thor thor = new Thor();
+        /*
+         * Thor thor = new Thor();
+         * 
+         * System.out.print("Ele é deus? (s/n) ");
+         * String verdade = input.next();
+         * 
+         * if (verdade.equals("s")) {
+         * thor.deus = true;
+         * } else if (verdade.equals("n")) {
+         * thor.deus = false;
+         * }
+         * 
+         * System.out.print("Ele tem martelo? ");
+         * verdade = input.next();
+         * 
+         * if (verdade.equals("s")) {
+         * thor.martelo = true;
+         * }
+         * else if (verdade.equals("n")) {
+         * thor.martelo = false;
+         * }
+         * 
+         * System.out.print("Ele é forte? ");
+         * verdade = input.next();
+         * 
+         * if (verdade.equals("s")) {
+         * thor.forte = true;
+         * }
+         * else if (verdade.equals("n")) {
+         * thor.forte = false;
+         * }
+         * 
+         * thor.voar();
+         * thor.invocarRaio();
+         */
 
-        System.out.print("Ele é deus? (s/n) ");
+        // Genética
+
+        /*
+         * Genetica genes = new Genetica();
+         * 
+         * System.out.print("DNA: ");
+         * genes.dna = input.next();
+         * System.out.print("Cromossomo: ");
+         * genes.cromossomo = input.next();
+         * 
+         * genes.herdar();
+         * 
+         * System.out.println("Após herdar, o bebe é: ");
+         * 
+         * genes.definirCaracteristicas();
+         */
+
+        /*
+         * Pintura pintura = new Pintura();
+         * 
+         * System.out.print("Autor: ");
+         * pintura.autor = input.next();
+         * System.out.print("Tipo de pintura: ");
+         * pintura.tipoPintura = input.next();
+         * System.out.print("Cor: ");
+         * pintura.cor = input.next();
+         * 
+         * pintura.expressar();
+         */
+
+        Celula celula = new Celula();
+
+        System.out.print("Nucleo? (s/n) ");
         String verdade = input.next();
 
         if (verdade.equals("s")) {
-            thor.deus = true;
+            celula.nucleo = true;
         } else if (verdade.equals("n")) {
-            thor.deus = false;
+            celula.nucleo = false;
         }
 
-        System.out.print("Ele tem martelo? ");
+        System.out.print("Membrana? (s/n) ");
         verdade = input.next();
 
         if (verdade.equals("s")) {
-            thor.martelo = true;
-        }
-        else if (verdade.equals("n")) {
-            thor.martelo = false;
+            celula.membrana = true;
+        } else if (verdade.equals("n")) {
+            celula.membrana = false;
         }
 
-        System.out.print("Ele é forte? ");
+        System.out.print("Cancerígena? (s/n) ");
         verdade = input.next();
 
         if (verdade.equals("s")) {
-            thor.forte = true;
+            celula.cancerigena = true;
+        } else if (verdade.equals("n")) {
+            celula.cancerigena = false;
         }
-        else if (verdade.equals("n")) {
-            thor.forte = false;
-        }
 
-        thor.voar();
-        thor.invocarRaio(); */
-
-        //Genética
-
-        /* Genetica genes = new Genetica();
-
-        System.out.print("DNA: ");
-        genes.dna = input.next();
-        System.out.print("Cromossomo: ");
-        genes.cromossomo = input.next();
-
-        genes.herdar();
-        
-        System.out.println("Após herdar, o bebe é: ");
-
-        genes.definirCaracteristicas();
-         */
-
-        Pintura pintura = new Pintura();
-
-        System.out.print("Autor: ");
-        pintura.autor = input.next();
-        System.out.print("Tipo de pintura: ");
-        pintura.tipoPintura = input.next();
-        System.out.print("Cor: ");
-        pintura.cor = input.next();
-
-        pintura.expressar();
+        celula.reproduzir();
     }
 }
