ecd88d7296b5e70cc09e4d1a569cb0c6ba8fb1f6
DutraGames <gabrieldutraamarante@gmail.com>
2023-09-29 18:05:09 -0300

    Create Class ArteDigital

---
+++ entidades/src/ArteDigital.java
@@ -0,0 +1,11 @@
+public class ArteDigital {
+    String autor;
+    float tamanho;
+    String formato;
+
+    void expressar() {
+        if (formato != null && tamanho != 0) {
+            System.out.println("Expressando uma Arte Digital!");
+        }
+    }
+}
