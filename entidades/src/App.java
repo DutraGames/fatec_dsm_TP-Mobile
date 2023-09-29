1b021cc7f5a3e39720f8f61dbc92161863da11fe
DutraGames <gabrieldutraamarante@gmail.com>
2023-09-28 22:07:15 -0300

    refactor: addd class professor

--- entidades/src/App.java
+++ entidades/src/App.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         Professor professor = new Professor();
         Scanner input = new Scanner(System.in);
+        Random rand = new Random();
 
-        professor.nome = "João";
-        professor.altura = 1.80f;
-        professor.formacao = "Mestrado";
+        System.out.print("Nome: ");
+        professor.nome = input.next();
+        System.out.print("Altura: ");
+        professor.altura = input.nextFloat();
+        System.out.print("Formacao: ");
+        professor.formacao = input.next();
 
+        System.out.println("O professor " + professor.nome + " ira ensinar!");
         professor.ensinar();
+        System.out.println("O professor " + professor.nome + " ira avaliar!");
+        int nota1 = rand.nextInt(10);
+        int nota2 = rand.nextInt(10);
+        int nota3 = rand.nextInt(10);
+        System.out.println("Sua media foi de: " + professor.avaliar(nota1, nota2, nota3));
+
+        
+        
     }
 }
