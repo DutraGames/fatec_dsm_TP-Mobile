541df96f76fb337405abf02c6f15978ff32285fb
DutraGames <gabrieldutraamarante@gmail.com>
2023-10-26 22:44:21 -0300

    fix: correction in class main

--- HERANCA/src/App.java
+++ HERANCA/src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import loja.*;;
 public class App {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         
