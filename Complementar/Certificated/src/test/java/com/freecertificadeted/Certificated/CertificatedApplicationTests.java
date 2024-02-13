d7e53de09bf4b8e3bdc8f764a2f5fe9b2e0e036d
DutraGames <gabrieldutraamarante@gmail.com>
2024-02-13 12:25:41 -0300

    chore: initial certificated COMPLEMENTAR

---
+++ Complementar/Certificated/src/test/java/com/freecertificadeted/Certificated/CertificatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freecertificadeted.Certificated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
