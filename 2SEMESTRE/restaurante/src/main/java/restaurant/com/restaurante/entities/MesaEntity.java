37a8628be61c1802e865ebf81a08b731a40e1b8e
DutraGames <gabrieldutraamarante@gmail.com>
2024-02-20 17:18:22 -0300

    feat: add Get, Put and Delete in Controller

--- 2SEMESTRE/restaurante/src/main/java/restaurant/com/restaurante/entities/MesaEntity.java
+++ 2SEMESTRE/restaurante/src/main/java/restaurant/com/restaurante/entities/MesaEntity.java
@@ -6,14 +6,17 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity(name = "mesa")
 public class MesaEntity {
-    @Id
-    @GeneratedValue
-    private int id;
-    private int numero;
-    private boolean livre = true;
+
+  @Id
+  @GeneratedValue
+  private int id;
+
+  private int numero;
+  private boolean livre = true;
 }
