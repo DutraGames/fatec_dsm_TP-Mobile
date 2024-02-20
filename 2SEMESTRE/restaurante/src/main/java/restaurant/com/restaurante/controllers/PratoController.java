ffa8c13092cada7b2b033d77820c96a27100bdf7
DutraGames <gabrieldutraamarante@gmail.com>
2024-02-20 16:13:06 -0300

    feat: add Put and Delete Controller

--- 2SEMESTRE/restaurante/src/main/java/restaurant/com/restaurante/controllers/PratoController.java
+++ 2SEMESTRE/restaurante/src/main/java/restaurant/com/restaurante/controllers/PratoController.java
@@ -2,13 +2,13 @@ package restaurant.com.restaurante.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import restaurant.com.restaurante.entities.PratoEntity;
 import restaurant.com.restaurante.repository.PratoRepository;
@@ -33,5 +33,24 @@ public class PratoController {
   @GetMapping("/{id}")
   public PratoEntity buscarUmPrato(@PathVariable int id) {
     return pratoRepository.findById(id).get();
+  }
+
+  @DeleteMapping("/{id}")
+  public String deletarPrato(@PathVariable int id) {
+    pratoRepository.deleteById(id);
+    return "Prato deletado";
+  }
+
+  @PutMapping("/{id}")
+  public PratoEntity atualizarPrato(
+    @PathVariable int id,
+    @RequestBody PratoEntity pratoNovo
+  ) {
+    PratoEntity pratoAtual = pratoRepository.findById(id).get();
+
+    pratoAtual.setNome(pratoNovo.getNome());
+    pratoAtual.setPreco(pratoNovo.getPreco());
+
+    return pratoRepository.save(pratoAtual);
   }
 }
