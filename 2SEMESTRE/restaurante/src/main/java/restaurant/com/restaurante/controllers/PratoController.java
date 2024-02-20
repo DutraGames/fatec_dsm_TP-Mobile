0372603e8fda4cffeaf46c40b8b95c0df564f366
DutraGames <gabrieldutraamarante@gmail.com>
2024-02-20 14:15:28 -0300

    add GEET for prato ID

--- 2SEMESTRE/restaurante/src/main/java/restaurant/com/restaurante/controllers/PratoController.java
+++ 2SEMESTRE/restaurante/src/main/java/restaurant/com/restaurante/controllers/PratoController.java
@@ -1,37 +1,37 @@
 package restaurant.com.restaurante.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-
-import restaurant.com.restaurante.entities.PratoEntity;
-import restaurant.com.restaurante.repository.PratoRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import restaurant.com.restaurante.entities.PratoEntity;
+import restaurant.com.restaurante.repository.PratoRepository;
 
 @RestController
 @RequestMapping("/prato")
 public class PratoController {
 
-    @Autowired
-    PratoRepository pratoRepository;
-    
-    @PostMapping()
-    public PratoEntity criarPrato(@RequestBody PratoEntity prato) {
-        return pratoRepository.save(prato);
-    }
+  @Autowired
+  PratoRepository pratoRepository;
 
-    @GetMapping()
-    public List<PratoEntity> buscarTodosOsPratos() {
-        return pratoRepository.findAll();
-    }
-    
-    
+  @PostMapping
+  public PratoEntity criarPrato(@RequestBody PratoEntity prato) {
+    return pratoRepository.save(prato);
+  }
+
+  @GetMapping
+  public List<PratoEntity> buscarTodosOsPratos() {
+    return pratoRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public PratoEntity buscarUmPrato(@PathVariable int id) {
+    return pratoRepository.findById(id).get();
+  }
 }
