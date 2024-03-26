99c575974e90fdcd23ef57d73ed243dc779982cc
DutraGames <gabrieldutraamarante@gmail.com>
2024-03-25 23:02:57 -0300

    feat: add create, update, delete and read tasks

--- 2SEMESTRE/ToDoList/src/main/java/tech/olutriz/todolist/ToDoList/ToDoListApplication.java
+++ 2SEMESTRE/ToDoList/src/main/java/tech/olutriz/todolist/ToDoList/ToDoListApplication.java
@@ -1,11 +1,14 @@
 package tech.olutriz.todolist.ToDoList;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import redis.clients.jedis.Connection;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 @SpringBootApplication
 public class ToDoListApplication implements CommandLineRunner {
@@ -16,12 +19,91 @@ public class ToDoListApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    String redisUrl = dotenv.get("REDIS_URL");
+    Scanner scanner = new Scanner(System.in);
 
-    Jedis jedis = new Jedis(redisUrl);
+    Jedis jedis = new Jedis(
+      "redis://default:Eksp4K0OpXDGjjMua3g5fuPXkgxKqkFW@redis-14724.c308.sa-east-1-1.ec2.cloud.redislabs.com:14724"
+    );
     Connection connection = jedis.getConnection();
 
-    System.out.println(connection.ping());
+    System.out.println("Connection: " + connection.ping());
+
+    try {
+      int opcao;
+      do {
+        System.out.println("\nEscolha uma opção:");
+        System.out.println("1. Adicionar Tarefa");
+        System.out.println("2. Listar Tarefas");
+        System.out.println("3. Listar Tarefas");
+        System.out.println("4. Marcar Tarefa Concluída");
+        System.out.println("5. Remover Tarefas Concluídas");
+        System.out.println("6. Sair");
+
+        opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+          case 1:
+            System.out.println("Digite o ID da tarefa:");
+            String id = scanner.nextLine();
+            System.out.println("Digite a descrição da tarefa:");
+            String descricao = scanner.nextLine();
+            jedis.hset("tarefas", id, descricao);
+            break;
+          case 2:
+            Map<String, String> tarefas = jedis.hgetAll("tarefas");
+            System.out.println("=================================");
+            System.out.println("Lista de Tarefas:");
+            for (Map.Entry<String, String> entry : tarefas.entrySet()) {
+              System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+            System.out.println("=================================");
+            break;
+          case 3:
+            Map<String, String> tarefasConcluídas = jedis.hgetAll(
+              "tarefas_concluidas"
+            );
+            System.out.println("=================================");
+            System.out.println("Lista de Tarefas Concluídas:");
+            for (Map.Entry<String, String> entry : tarefasConcluídas.entrySet()) {
+              System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+            System.out.println("=================================");
+            break;
+          case 4:
+            System.out.println("Digite o ID da tarefa concluída:");
+            String idConcluida = scanner.nextLine();
+            jedis.hset(
+              "tarefas_concluidas",
+              idConcluida,
+              jedis.hget("tarefas", idConcluida)
+            );
+            jedis.hdel("tarefas", idConcluida);
+            break;
+          case 5:
+            Set<String> idsTarefasConcluidas = jedis.hkeys(
+              "tarefas_concluidas"
+            );
+            for (String iditificated : idsTarefasConcluidas) {
+              jedis.hdel("tarefas_concluidas", iditificated);
+            }
+            break;
+          case 6:
+            System.out.println("Saindo...");
+            break;
+          default:
+            System.out.println("Opção inválida.");
+        }
+      } while (opcao != 5);
+    } catch (JedisConnectionException e) {
+      System.err.println(
+        "Não foi possível conectar ao servidor Redis. Verifique suas credenciais e conexão com a internet."
+      );
+    } finally {
+      jedis.close();
+      scanner.close();
+    }
+
+    jedis.close();
   }
 }
