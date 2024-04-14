79d04afb3b3a491266a530097bdfa3d2dbd827fc
DutraGames <gabrieldutraamarante@gmail.com>
2024-04-14 10:59:15 -0300

    feat: add service class for API

---
+++ 2SEMESTRE/EmailSend/src/main/java/br/com/testemail/marketing/EmailSend/API/services/ConsomeAPI.java
@@ -0,0 +1,24 @@
+package br.com.testemail.marketing.EmailSend.API.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsomeAPI {
+
+  public String lerJson(String url) {
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+
+    HttpResponse<String> response;
+
+    try {
+      response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    } catch (IOException | InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    return response.body();
+  }
+}
