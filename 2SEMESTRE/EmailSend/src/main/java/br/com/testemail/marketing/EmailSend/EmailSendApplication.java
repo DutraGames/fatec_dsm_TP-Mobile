a3b16cddb1d842bb437e8724469497a0f2580a54
DutraGames <gabrieldutraamarante@gmail.com>
2024-04-14 10:51:27 -0300

    init: create project

---
+++ 2SEMESTRE/EmailSend/src/main/java/br/com/testemail/marketing/EmailSend/EmailSendApplication.java
@@ -0,0 +1,13 @@
+package br.com.testemail.marketing.EmailSend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSendApplication.class, args);
+	}
+
+}
---
+++ 2SEMESTRE/EmailSend/src/test/java/br/com/testemail/marketing/EmailSend/EmailSendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testemail.marketing.EmailSend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
