4ae93a07fbbb8c0819c233efe6d22878093bc4ba
DutraGames <gabrieldutraamarante@gmail.com>
2024-04-16 09:34:24 -0300

    feat: add call in API

--- 2SEMESTRE/EmailSend/src/main/java/br/com/testemail/marketing/EmailSend/EmailSendApplication.java
+++ 2SEMESTRE/EmailSend/src/main/java/br/com/testemail/marketing/EmailSend/EmailSendApplication.java
@@ -1,13 +1,24 @@
 package br.com.testemail.marketing.EmailSend;
 
+import br.com.testemail.marketing.EmailSend.API.services.ConsomeAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailSendApplication {
+public class EmailSendApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmailSendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EmailSendApplication.class, args);
+  }
 
+  @Override
+  public void run(String... args) throws Exception {
+    ConsomeAPI consomeApi = new ConsomeAPI();
+    String json = consomeApi.lerJson(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+
+    System.out.println(json);
+  }
 }
