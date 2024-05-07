f81b63ef50db203d265109e34aa78f936bdbef19
DutraGames <gabrieldutraamarante@gmail.com>
2024-05-06 22:58:26 -0300

    feat: add relations in entities

--- 2SEMESTRE/departfirm/src/main/java/br/edu/fatecpg/departfirm/models/Department.java
+++ 2SEMESTRE/departfirm/src/main/java/br/edu/fatecpg/departfirm/models/Department.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Department {
@@ -13,6 +16,9 @@ public class Department {
   private long id;
 
   private String name;
+
+  @OneToMany(mappedBy = "department")
+  private List<Employee> employees = new ArrayList<>();
 
   public Department() {}
 
@@ -26,5 +32,27 @@ public class Department {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public List<Employee> getEmployees() {
+    return employees;
+  }
+
+  public void setEmployees(Employee employees) {
+    this.employees.add(employees);
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "Department [id=" +
+      id +
+      ", name=" +
+      name +
+      ", employees=" +
+      employees +
+      "" +
+      "]"
+    );
   }
 }
--- 2SEMESTRE/departfirm/src/main/java/br/edu/fatecpg/departfirm/models/Employee.java
+++ 2SEMESTRE/departfirm/src/main/java/br/edu/fatecpg/departfirm/models/Employee.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Employee {
@@ -13,6 +14,9 @@ public class Employee {
   private long id;
 
   private String name;
+
+  @ManyToOne
+  private Department department;
 
   public Employee() {}
 
@@ -26,5 +30,27 @@ public class Employee {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Department getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(Department department) {
+    this.department = department;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "Employee [id=" +
+      id +
+      ", name=" +
+      name +
+      ", department=" +
+      department +
+      "" +
+      "]"
+    );
   }
 }
