18b13efe21e7972a66d97dc8089181a130b5f926
DutraGames <gabrieldutraamarante@gmail.com>
2024-05-06 21:46:21 -0300

    first commit: init project DepartFirm

---
+++ 2SEMESTRE/departfirm/src/test/java/br/edu/fatecpg/departfirm/DepartFirmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.departfirm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartFirmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
