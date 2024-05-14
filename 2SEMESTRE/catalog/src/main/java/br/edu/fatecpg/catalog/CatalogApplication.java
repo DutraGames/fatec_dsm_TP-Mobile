b35802ac175a594dcb85fe116215f938dea3feeb
DutraGames <gabrieldutraamarante@gmail.com>
2024-05-14 19:48:25 -0300

    create project for Catalog

---
+++ 2SEMESTRE/catalog/src/main/java/br/edu/fatecpg/catalog/CatalogApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}
---
+++ 2SEMESTRE/catalog/src/test/java/br/edu/fatecpg/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
