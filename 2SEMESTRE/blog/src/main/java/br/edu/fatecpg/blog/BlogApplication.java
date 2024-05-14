173018fc2740a119c60a25b96490f2faa2015a23
DutraGames <gabrieldutraamarante@gmail.com>
2024-05-14 18:58:46 -0300

    create project Blog

---
+++ 2SEMESTRE/blog/src/main/java/br/edu/fatecpg/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
