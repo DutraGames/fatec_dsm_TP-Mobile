e178293b9b0c9955331bc7144cda47ea8e4178d0
DutraGames <gabrieldutraamarante@gmail.com>
2024-05-14 19:21:39 -0300

    feat: add Post Repository

---
+++ 2SEMESTRE/blog/src/main/java/br/edu/fatecpg/blog/repositories/PostRepository.java
@@ -0,0 +1,6 @@
+package br.edu.fatecpg.blog.repositories;
+
+import br.edu.fatecpg.blog.Models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {}
