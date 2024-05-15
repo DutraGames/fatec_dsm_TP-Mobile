c29871c1345a4e9491cb688ec34b220c13091160
DutraGames <gabrieldutraamarante@gmail.com>
2024-05-14 21:26:31 -0300

    feat: add data example

--- 2SEMESTRE/catalog/src/main/java/br/edu/fatecpg/catalog/CatalogApplication.java
+++ 2SEMESTRE/catalog/src/main/java/br/edu/fatecpg/catalog/CatalogApplication.java
@@ -1,13 +1,46 @@
 package br.edu.fatecpg.catalog;
 
+import br.edu.fatecpg.catalog.models.Category;
+import br.edu.fatecpg.catalog.models.Product;
+import br.edu.fatecpg.catalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogApplication {
+public class CatalogApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogApplication.class, args);
-	}
+  @Autowired
+  private CategoryRepository categoryRepository;
 
+  public static void main(String[] args) {
+    SpringApplication.run(CatalogApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    /*     Category category = new Category("Smartphones");
+    Category category2 = new Category("Games");
+
+    Product product = new Product("iPhone 14", 3500.0);
+    Product product2 = new Product("Xiaomi 12s", 1600.0);
+    Product product3 = new Product("Xbox Series X", 4000.0);
+    Product product4 = new Product("PlayStation 5", 4000.0);
+
+    category.setProducts(product);
+    category.setProducts(product2);
+    category2.setProducts(product3);
+    category2.setProducts(product4);
+
+    categoryRepository.save(category);
+    categoryRepository.save(category2); */
+
+    System.out.println("--------------------------------------------------");
+    System.out.println("All Categories: ");
+
+    categoryRepository.findAll().forEach(System.out::println);
+
+    System.out.println("--------------------------------------------------");
+  }
 }
